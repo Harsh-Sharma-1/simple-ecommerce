a79bb6e8660d267c9b9ca8a5d473a839bddee4fd
Harsh-Sharma-1 <85671456+Harsh-Sharma-1@users.noreply.github.com>
2022-02-16 17:26:26 +0530

    added router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Routes } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 export const App = () => {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 };
