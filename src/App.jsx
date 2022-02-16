79ea12d2281cc64e15807fc57314f77911a28866
Harsh-Sharma-1 <85671456+Harsh-Sharma-1@users.noreply.github.com>
2022-02-16 18:20:58 +0530

    shop page ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Routes } from "react-router-dom";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
+import "./App.css";
 
 export const App = () => {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
